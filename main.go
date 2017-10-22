3cadc69f9bc0a2a7d6dff9453085dfe0a862fe96
Jason Willems <jason@willems.ca>
2017-10-21 21:51:24 -0700

    Read from json

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main 
 
 import (
+  "encoding/json"
   "log"
   "net/http"
+  "strings"
 
   "github.com/lestrrat/go-libxml2"
   "github.com/lestrrat/go-libxml2/types"
@@ -10,13 +12,57 @@ import (
 )
 
 
+type Price struct {
+  Date    string
+  Price   string
+}
+
+type Item struct {
+  Name    string
+  Link    string
+  Prices  []Price
+}
+
+type Config struct {
+  Items []Item
+}
+
+
+var itemList = `{
+  "items": [
+    {
+      "name": "Seagate 8 TB Hard Drive",
+      "link": "https://www.amazon.com/Seagate-Expansion-Desktop-External-STEB8000100/dp/B01HAPGEIE",
+      "prices": []
+    },
+    {
+      "name": "Western Digital 8 TB Hard Drive",
+      "link": "https://www.amazon.com/dp/B01LQQHLGC/ref=twister_B0751SCZW7",
+      "prices": []
+    }
+  ]
+}`
+
+
+
 func main() {
 
-  link := "https://www.amazon.com/Seagate-Expansion-Desktop-External-STEB8000100/dp/B01HAPGEIE/ref=sr_1_3?ie=UTF8&qid=1508395970&sr=8-3&keywords=8tb+hard+drive"
+  log.Printf("Fetching today's prices...")
+  productLinkMap := getProductList()
 
+  for name, link := range productLinkMap {
+    price := getPrice(name, link)
+    log.Printf(`Today's price for "%s" is %s`, name, price)  
+  }
+}
+
+
+func getPrice(item_name string, link string) string {
+
+  site := strings.Split(link, "/")[2]
   res, err := http.Get(link)
   if err != nil {
-    panic("Failed to retrieve page at www.amazon.com: " + err.Error())
+    panic("Failed to retrieve page at : " + site + " => " + err.Error())
   }
 
 
@@ -34,10 +80,25 @@ func main() {
 
 
   target_xpath := `//*[@id="priceblock_ourprice"]`
-
   text := xpath.String(doc.Find(target_xpath))
 
-  log.Printf("Price is %s", text)
+  return text
 }
 
+
+func getProductList() map[string]string {
+  var conf Config
+  err := json.Unmarshal([]byte(itemList), &conf)
+
+  if err != nil {
+    panic("Failed to read JSON " + err.Error())
+  }
+
+  name_link := make(map[string]string)
+  for item := range conf.Items {
+    name_link[conf.Items[item].Name] = conf.Items[item].Link
+  }
+  
+  return name_link
+}
 
